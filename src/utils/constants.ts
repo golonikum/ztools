1d504b9c08af138bec31e56bdd68adc0d5e9d418
Голов Никита Викторович <n.golov@zetra.pro>
2025-12-24 13:43:38 +0300

    Added project-widgets

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -11,6 +11,7 @@ export const PROJECTS_SRC_MAP = {
   "admin-ui": "GIT_DOMAIN/kub/admin/admin-ui",
   "inventory-ui": "GIT_DOMAIN/kub/inventory/ui/inventory-ui",
   "pas-ui": "GIT_DOMAIN/kub/forecast/ui/pas-ui.git",
+  "project-widgets": "GIT_DOMAIN/kub/man/ui/project-widgets.git",
 };
 
 export const PROJECTS_COMMANDS_MAP: Partial<
